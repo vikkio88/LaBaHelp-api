5cdcfe92b02bebaaac8adf8e83a95d5ab4224a12
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2018-01-28 18:25:18 +0000

    using cvreader on action

--- Actions/Elaborate.php
+++ Actions/Elaborate.php
@@ -4,35 +4,24 @@ namespace App\Actions;
 
 use App\Lib\Helpers\Config;
 use App\Lib\Slime\RestAction\ApiAction;
+use App\Models\Lib\CvReader;
 use App\Models\Log;
 
 class Elaborate extends ApiAction
 {
     protected function performAction()
     {
-        $text = $this->getJsonRequestBody()['text'];
         $body = $this->getJsonRequestBody();
-        $words = str_word_count($body['text'], 1);
-        $exceptions = Config::get('exceptions.list');
-        $words = array_filter($words, function ($word) use ($exceptions) {
-            return strlen($word) > 1 && !in_array(strtolower($word), $exceptions);
-        });
-        $counting = [];
-        foreach ($words as $word) {
-            if (isset($counting[$word])) {
-                continue;
-            }
-            $counting[$word] = substr_count($text, $word);
-        }
-        arsort($counting);
-        $result = [];
-        foreach ($counting as $value => $count) {
-            $result[] = [
-                'value' => $value,
-                'count' => $count
-            ];
-        }
-
+        $text = $body['text'];
+        $cvReader = new CvReader(
+            $text,
+            Config::get('map.config'),
+            [
+                'words' => Config::get('exceptions.words'),
+                'punctuation' => Config::get('exceptions.punctuation')
+            ]
+        );
+        $result = $cvReader->skim();
         try {
             Log::create(array_merge(
                 $body,
